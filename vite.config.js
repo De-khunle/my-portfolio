619549890decad78712569f329e5a88f052aa879
De-khunle <popoolahabeeb9@gmail.com>
2026-06-11 22:42:05 +0100

    Fix: Vite base config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-portfolio/", // ADDED THIS LINE FOR GIT TO RUN PROPERLY
+  base: "./", // Vite base config
 });
